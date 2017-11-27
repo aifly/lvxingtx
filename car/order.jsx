fd3c5699bad6335f294a3d42658de54c7c5683d0
clearinfo <songxian@163.com>
2017-11-27 12:11:44 +0800

    song

--- car/order.jsx
+++ car/order.jsx
@@ -33,19 +33,71 @@ class ZmitiCarorderApp extends React.Component {
                             <NavBar mode="light">确认订单</NavBar>
                             <div className="hr10"></div>
                             <div className="lv-pane-orderview">
-                                <div className="lv-pane-orderview-inner">
-                                  
-                                  
+                                <div className="lv-pane-orderview-inner lv-pane-orderview-column1">                                  
+                                  <List>
+                                    <Item>舒驰YTK6810EV1纯电动客车8米</Item>
+                                    <Item
+                                      thumb="./assets/images/car-05.png"
+                                    >
+                                        <div className="lv-pane-orderview-arr">
+                                            <div className="lv-pane-orderview-arritem">
+                                                <div className="lv-pane-orderview-arr-l">电动机型</div>
+                                                <div className="lv-pane-orderview-arr-r">WTEM60-40-2</div>
+                                            </div>
+                                            <div className="lv-pane-orderview-arritem2">
+                                                <div className="lv-pane-orderview-arr-l">电池总储电量</div>
+                                                <div className="lv-pane-orderview-arr-r">122kwh</div>
+                                            </div>
+                                            <div className="lv-pane-orderview-arritem">
+                                                <div className="lv-pane-orderview-arr-l">续航里程</div>
+                                                <div className="lv-pane-orderview-arr-r">260km</div>
+                                            </div>
+                                            <div className="lv-pane-orderview-arritem">
+                                                <div className="lv-pane-orderview-arr-l">可乘人数</div>
+                                                <div className="lv-pane-orderview-arr-r">26</div>
+                                            </div>
+                                        </div>
+
+                                    </Item>
+                                  </List>
 
                                   
-                                  
                                 </div>
+                                <div className="hr10"></div>
+                                <div className="lv-pane-orderview-inner lv-pane-orderview-column2">
+                                    <List>
+                                        <Item>
+                                            地区
+                                        </Item>
+                                        <Item>
+                                            门店
+                                        </Item>
+                                    </List>
+                                </div>
+                                <div className="hr10"></div>
+                                <div className="lv-pane-orderview-inner lv-pane-orderview-column2">
+                                    <List>
+                                        <Item>
+                                            姓名
+                                        </Item>
+                                        <Item>
+                                            电话
+                                        </Item>
+                                        <Item>
+                                            身份证号
+                                        </Item>
+                                    </List>
+                                </div>
+                                
                                
                             </div>
                         </div>
                     </div>
                 </div>
-
+                <div className="lv-pane-orderview-submit">
+                    <div className="lv-pane-orderview-submit-l">确认信息无误后可提交订单</div>
+                    <div className="lv-pane-orderview-submit-r">提交订单</div>
+                </div>
                   
             </div>
         )
